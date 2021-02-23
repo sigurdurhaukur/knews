54f260c60e8bb825024a95a5c825a622656d94ad
sigurdurhaukur <sigurdur.h.b@icloud.com>
2021-02-23 17:18:18 +0000

    added more news filters
    plus cleaned up the home page

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -57,3 +57,147 @@ module.exports.settings_visir = (req, res) => {
 		})
 		.catch((err) => res.status(422).json(err));
 };
+
+module.exports.settings_man = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.man': { $eq: [false, '$settings.man'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_fotbolti = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.fotbolti': { $eq: [false, '$settings.fotbolti'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_dv = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.dv': { $eq: [false, '$settings.dv'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_klippa = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.klippa': { $eq: [false, '$settings.klippa'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_kjarninn = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.kjarninn': { $eq: [false, '$settings.kjarninn'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_ruv = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.ruv': { $eq: [false, '$settings.ruv'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_stundin = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.stundin': { $eq: [false, '$settings.stundin'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_kaktus = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.kaktus': { $eq: [false, '$settings.kaktus'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
+
+module.exports.settings_ron = (req, res) => {
+	const _email = req.body.email;
+	const field = req.body.field;
+	const email = _email.replace('Daginn, ', '');
+	console.log(email);
+
+	User.updateOne({ email: email }, [
+		{ $set: { 'settings.ron': { $eq: [false, '$settings.ron'] } } },
+	])
+		.then((dbModel) => {
+			res.json(dbModel);
+			res.redirect('menu');
+		})
+		.catch((err) => res.status(422).json(err));
+};
--- public/js/menu.js
+++ public/js/menu.js
@@ -2,6 +2,16 @@ const email = document.querySelector('a.email').innerText;
 const fb = document.querySelector('.fb');
 const mbl = document.querySelector('.mbl');
 const visir = document.querySelector('.visir');
+const man = document.querySelector('.man');
+const fotbolti = document.querySelector('.fotbolti');
+
+const dv = document.querySelector('.dv');
+const klippa = document.querySelector('.klippa');
+const kjarninn = document.querySelector('.kjarninn');
+const ruv = document.querySelector('.ruv');
+const stundin = document.querySelector('.stundin');
+const kaktus = document.querySelector('.kaktus');
+const ron = document.querySelector('.ron');
 
 fb.addEventListener('click', function () {
 	post('/settings/fb');
@@ -11,6 +21,33 @@ mbl.addEventListener('click', function () {
 });
 visir.addEventListener('click', function () {
 	post('/settings/visir');
+});
+man.addEventListener('click', function () {
+	post('/settings/man');
+});
+fotbolti.addEventListener('click', function () {
+	post('/settings/fotbolti');
+});
+dv.addEventListener('click', function () {
+	post('/settings/dv');
+});
+klippa.addEventListener('click', function () {
+	post('/settings/klippa');
+});
+kjarninn.addEventListener('click', function () {
+	post('/settings/kjarninn');
+});
+ruv.addEventListener('click', function () {
+	post('/settings/ruv');
+});
+stundin.addEventListener('click', function () {
+	post('/settings/stundin');
+});
+kaktus.addEventListener('click', function () {
+	post('/settings/kaktus');
+});
+ron.addEventListener('click', function () {
+	post('/settings/ron');
 });
 
 function post(route) {
